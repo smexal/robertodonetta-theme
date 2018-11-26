3d7fa8e2ee53782f5655fcf1ac30cfef747f83e0
Silas Mächler <silas.maechler@gmail.com>
2018-11-26 20:50:42 +0100

    new template

---
+++ templates/collections/cache/documentation.19979665364f9c79ab9ffb53048d4c60.rtpl.php
@@ -0,0 +1,16 @@
+<?php if(!class_exists('raintpl')){exit;}?><div class="hero">
+</div>
+<div class="intro-infos">
+    <div class="wrapped">
+        <div class="row">
+            <div class="col-lg-6 col-sm-12">
+                <h1 style="padding-top: 40px"><?php echo $title;?></h1>
+                <div class="text">
+                    <p class="lead"><?php echo $description;?></p>
+                    <?php echo $text;?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
